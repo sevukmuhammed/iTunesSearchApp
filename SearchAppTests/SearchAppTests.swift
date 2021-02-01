305f4830456a8bac09cbb854d132e687579a83e0
Muhammed Sevuk <sevukmuhammed@gmail.com>
2021-02-01 14:05:40 +0300

    Test update

--- SearchAppTests/SearchAppTests.swift
+++ SearchAppTests/SearchAppTests.swift
@@ -7,55 +7,55 @@
 
 import XCTest
 import SearchApp
-//import RxSwift
-//import RxTest
-//import RxBlocking
+import RxSwift
+import RxTest
+import RxBlocking
 
 @testable import SearchApp
-//@testable import RxTest
-//@testable import RxSwift
-//@testable import RxBlocking
+@testable import RxTest
+@testable import RxSwift
+@testable import RxBlocking
 
 class SearchAppTests: XCTestCase {
     
-//    var viewModel: SearchPageViewModel!
-//    var disposeBag: DisposeBag!
-//    var scheduler: TestScheduler!
+    var viewModel: SearchPageViewModel!
+    var disposeBag: DisposeBag!
+    var scheduler: TestScheduler!
     
     override func setUpWithError() throws {
-//        self.scheduler = TestScheduler(initialClock: 0)
-//        self.disposeBag = DisposeBag()
-//        self.viewModel = SearchPageViewModel(disposeBag: self.disposeBag, SearchPageRepository: SearchPageRepository())
+        self.scheduler = TestScheduler(initialClock: 0)
+        self.disposeBag = DisposeBag()
+        self.viewModel = SearchPageViewModel(disposeBag: self.disposeBag, SearchPageRepository: SearchPageRepository())
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-//    func testFileManager() throws {
-//        guard let items = MockService.readJson() else {return}
-//
-//        viewModel.behaviorRelay.accept(items)
-//        let trackName = try viewModel.behaviorRelay.toBlocking().first()?.results?.first?.trackName
-//        XCTAssertEqual(trackName, "Harry Potter and the Deathly Hallows, Part 2")
-//
-//    }
+    func testFileManager() throws {
+        guard let items = MockService.readJson() else {return}
+
+        viewModel.behaviorRelay.accept(items)
+        let trackName = try viewModel.behaviorRelay.toBlocking().first()?.results?.first?.trackName
+        XCTAssertEqual(trackName, "Harry Potter and the Deathly Hallows, Part 2")
+
+    }
     
-//    func testRelays() {
-//        guard let data = MockService.readJson() else {return}
-//
-//        // create scheduler
-//        let rates = scheduler.createObserver(Response.self)
-//
-//        viewModel.behaviorRelay.asDriver().drive(rates).disposed(by: disposeBag)
-//
-//        let coldObservable = scheduler.createColdObservable([.next(10, data.results?.first),.next(20, data.results?.last)])
-//
-//        scheduler.start()
-//
-//        XCTAssertEqual(coldObservable.recordedEvents.first!.value.element!?.trackName, "Harry Potter and the Deathly Hallows, Part 2")
-//        XCTAssertEqual(coldObservable.recordedEvents.last!.value.element!?.trackName, "Harry Connick, Jr.: Lyrics to Live By")
-//    }
+    func testRelays() {
+        guard let data = MockService.readJson() else {return}
+
+        // create scheduler
+        let rates = scheduler.createObserver(Response.self)
+
+        viewModel.behaviorRelay.asDriver().drive(rates).disposed(by: disposeBag)
+
+        let coldObservable = scheduler.createColdObservable([.next(10, data.results?.first),.next(20, data.results?.last)])
+
+        scheduler.start()
+
+        XCTAssertEqual(coldObservable.recordedEvents.first!.value.element!?.trackName, "Harry Potter and the Deathly Hallows, Part 2")
+        XCTAssertEqual(coldObservable.recordedEvents.last!.value.element!?.trackName, "Harry Connick, Jr.: Lyrics to Live By")
+    }
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
